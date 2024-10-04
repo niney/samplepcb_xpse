17c3bf859083566e11de0c79dc6497f6a1cb133c
niney <kpeteras@gmail.com>
2024-10-04 23:22:37 +0900

    Initialize Spring Boot project with Gradle support
    .gitignore 파일 추가, 기본 Gradle 설정 구성, 및 Spring Boot 애플리케이션 기본 구조를 생성했습니다. 이는 프로젝트 기본 설정 및 의존성 관리를 용이하게 합니다.

---
+++ src/main/java/kr/co/samplepcb/xpse/SamplepcbXpseApplication.java
@@ -0,0 +1,13 @@
+package kr.co.samplepcb.xpse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplepcbXpseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamplepcbXpseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/samplepcb/xpse/SamplepcbXpseApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.samplepcb.xpse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplepcbXpseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

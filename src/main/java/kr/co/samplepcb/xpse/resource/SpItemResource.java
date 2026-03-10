b36c0d741c673225dd369ec84f40168e431b7bfe
niney <kpeteras@gmail.com>
2026-03-11 02:11:48 +0900

    feat security > SpItem, SpOrder 조회 API에 JWT 인증 추가
    SpItemResource와 SpOrderResource의 API 엔드포인트에 @JwtAuth 어노테이션을 적용하여 JWT 인증을 강화했습니다. 이를 통해 API 호출 시 사용자 인증 절차를 거치도록 개선했습니다.

--- src/main/java/kr/co/samplepcb/xpse/resource/SpItemResource.java
+++ src/main/java/kr/co/samplepcb/xpse/resource/SpItemResource.java
@@ -1,6 +1,7 @@
 package kr.co.samplepcb.xpse.resource;
 
 import coolib.common.CCResult;
+import kr.co.samplepcb.xpse.security.JwtAuth;
 import kr.co.samplepcb.xpse.service.SpItemService;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +15,7 @@ public class SpItemResource {
         this.spItemService = spItemService;
     }
 
+    @JwtAuth
     @GetMapping("/{itId}")
     public CCResult getDetail(@PathVariable String itId) {
         return this.spItemService.getDetail(itId);
--- src/main/java/kr/co/samplepcb/xpse/resource/SpOrderResource.java
+++ src/main/java/kr/co/samplepcb/xpse/resource/SpOrderResource.java
@@ -2,6 +2,7 @@ package kr.co.samplepcb.xpse.resource;
 
 import coolib.common.CCResult;
 import kr.co.samplepcb.xpse.pojo.SpOrderSearchParam;
+import kr.co.samplepcb.xpse.security.JwtAuth;
 import kr.co.samplepcb.xpse.service.SpOrderService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,7 @@ public class SpOrderResource {
         this.spOrderService = spOrderService;
     }
 
+    @JwtAuth
     @GetMapping("/_search")
     public CCResult search(Pageable pageable, SpOrderSearchParam searchParam) {
         return this.spOrderService.search(pageable, searchParam);
